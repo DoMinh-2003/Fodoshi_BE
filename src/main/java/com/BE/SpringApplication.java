1b2bd1f4e204faed1bc868e010170c755abab41f
DoMinh-2003 <minhlola28@gmail.com>
2025-02-28 11:12:16 +0700

    feat: setup payos

--- src/main/java/com/BE/SpringApplication.java
+++ src/main/java/com/BE/SpringApplication.java
@@ -5,13 +5,19 @@ import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @EnableCaching
-@OpenAPIDefinition(info = @Info(title = "Do Minh API", version = "2.0", description = "Information"))
 @SecurityScheme(name = "api", scheme = "bearer", type = SecuritySchemeType.HTTP, in = SecuritySchemeIn.HEADER)
+@OpenAPIDefinition(
+		info = @Info(title = "Do Minh API", version = "2.0"),
+		servers = {
+				@Server(url = "https://fodoshi.shop", description = "Production Server") // Sử dụng HTTPS
+		}
+)
 public class SpringApplication {
 	public static void main(String[] args) {
 		org.springframework.boot.SpringApplication.run(SpringApplication.class,args);
---
+++ src/main/java/com/BE/model/request/CreatePaymentLinkRequestBody.java
@@ -0,0 +1,18 @@
+package com.BE.model.request;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class CreatePaymentLinkRequestBody {
+    private String productName;
+    private String description;
+    private String returnUrl;
+    private int price;
+    private String cancelUrl;
+
+}
